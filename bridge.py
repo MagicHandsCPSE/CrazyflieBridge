c2b7bbdc35c7685335b573b5ffa96f3cd90a6b73
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2023-08-04 10:53:10 -0400

    add protocol code

--- bridge.py
+++ bridge.py
@@ -5,39 +5,74 @@ from threading import Event
 
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
-from cflib.crazyflie.log import LogConfig
 from cflib.crazyflie.syncCrazyflie import SyncCrazyflie
 from cflib.positioning.motion_commander import MotionCommander
+from cflib.utils.multiranger import Multiranger
 from cflib.utils import uri_helper
+
+import serial
+import asyncio
 
 
 URI = uri_helper.uri_from_env(default='radio://0/80/2M/E7E7E7E7E7')
-flowdeck_attached = Event()
 
 DEFAULT_HEIGHT = 0.5
 
+def is_close(reading):
+    return reading is None or reading < 0.3  # 30 cm = about 1 foot
 
-def flow_deck_callback(_, value_str):
-    value = int(value_str)
-    print(value, end=" => ")
-    if value:
-        flowdeck_attached.set()
-        print('Deck is attached!')
-    else:
-        print('Deck is NOT attached!')
+queue = asyncio.Queue()
 
-def main(scf):
-    with MotionCommander(scf, default_height=DEFAULT_HEIGHT) as mc:
-        try:
-            time.sleep(3)
-        finally:
-            mc.stop()
+async def readserial(port):
+    try:
+        while True:
+            what, where = port.readline().decode("ascii").split("=")
+            await queue.put((what, int(where)))
+    finally:
+        port.close()
+
+async def fly(scf, mr, mc):
+    vx, vy, vz = 0, 0, 0
+    MAX_VEL = 0.5
+    try:
+        while True:
+            try:
+                what, where = queue.get_nowait()
+            except asyncio.QueueEmpty:
+                mc.stop()
+                vx, vy, vz = 0, 0, 0
+                continue
+            queue.task_done()
+            match what:
+                case "x":
+                    vx = where * MAX_VEL / 90
+                case "y":
+                    vy = where * MAX_VEL / 90
+                case "a":
+                    vz = where * MAX_VEL / 2
+            # Safety guards
+            if is_close(mr.front) and vx > 0:
+                vx = 0
+            if is_close(mr.back) and vx < 0:
+                vx = 0
+            if is_close(mr.left) and vy > 0:
+                vy = 0
+            if is_close(mr.right) and vy < 0:
+                vy = 0
+            if is_close(mr.up) and vz > 0:
+                vz = 0
+            if is_close(mr.down) and vz < 0:
+                vz = 0
+            mc.start_linear_motion(vx, vy, vz)
+    finally:
+        mc.stop()
+
+async def main(scf, mr, mc):
+    await asyncio.gather(fly(scf, m, mc), readserial(port))
 
 if __name__ == '__main__':
     cflib.crtp.init_drivers()
+    port = Serial("/dev/ttyAMA0", 115200, timeout=0.1, exclusive=True)
     with SyncCrazyflie(URI, cf=Crazyflie(rw_cache='./cache')) as scf:
-        scf.cf.param.add_update_callback(group="deck", name="bcFlow2", cb=flow_deck_callback)
-        if not flowdeck_attached.wait(timeout=10):
-            print('error: flow deck not detected')
-            sys.exit(1)
-        main(scf)
+        with Multiranger(scf) as multiranger, MotionCommander(scf) as motion_commander:
+            asyncio.main(main(scf, multiranger, motion_commander, port))
